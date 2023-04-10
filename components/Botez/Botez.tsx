79dc7b4aec300fb4b19350952676196218c41025
0xInu <calin.cretiu@gmail.com>
2023-04-10 08:50:19 +0300

    'responsiveness'

---
+++ components/Botez/Botez.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React, { useEffect, useState } from "react";
+type Props = {};
+
+function Botez(props: Props) {
+  const [aiName, setAiName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [loaded, isLoaded] = useState(false);
+  async function getAnswer() {
+    setLoading(true);
+    const aiRes = await fetch("./api/hello");
+    const aiName = await aiRes.json();
+    setAiName(aiName.name);
+    setLoading(false);
+    isLoaded(true);
+    console.log(aiName.name);
+  }
+  useEffect(() => {
+    // getAnswer();
+    console.log(
+      aiName.indexOf("este "),
+      aiName.indexOf(",", aiName.indexOf("este "))
+    );
+    console.log(aiName.substring(0, aiName.indexOf("este") + 4));
+  }, []);
+
+  function splitText(str: string) {
+    const start = str.indexOf("este");
+    const end = str.search(/[,.;:!?]/);
+    const before = str.substring(0, start + 4);
+    const name = str.substring(start, end).trim();
+    const after = str.substring(end, str.length);
+    return [before, end, after];
+  }
+
+  return (
+    <div className="flex items-start justify-start flex-col gap-2 bg-black max-w-2xl">
+      <div className="text-xs sm:text-sm md:text-base lg:text-lg dark:text-white mb-2">
+        Bo
+        <span className="animate-bounce transition delay-150 duration-300 ease-in-out">
+          t
+        </span>
+        ez
+      </div>
+      <div className="text-xs sm:text-sm md:text-base lg:text-lg bg-clip-text text-transparent bg-gradient-to-r from-teal-500 to-blue-500 opacity-70">
+        Ajuta pe ChatGPT sa isi aleaga un nume
+      </div>
+      {aiName !== "" ? (
+        <div className="text-xl text-white mt-2 text-xs sm:text-sm md:text-base lg:text-lg">
+          {aiName}
+        </div>
+      ) : (
+        <></>
+      )}
+      <div className="flex justify-center text-white font-bold my-4">
+        <button
+          disabled={loading}
+          className="disabled:bg-green-50  bg-clip-border bg-gradient-to-r from-teal-400 to-blue-400 
+          opacity-40 animate-pulse px-6 py-2 rounded-sm text-slate-200"
+
+          onClick={getAnswer}
+        >
+          {!loading && !aiName ? "Boteaza" : !loading && "rebot"}
+          {loading && (
+            <div className="animate-spin text-black viewBox='0 0 24 24'">߷</div>
+          )}
+        </button>{" "}
+      </div>
+    </div>
+  );
+}
+
+export default Botez;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { OpenAI } from "langchain/llms";
+import { ConversationChain } from "langchain/chains";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const model = new OpenAI({ openAIApiKey:process.env.OPENAI_API_KEY, temperature: 0.9 });
+  
+  const chain = new ConversationChain({ llm: model });
+  console.log(req);
+  let res4 = await chain.call({
+    input:
+      "a compassionate Human speaks: Hi! Your name is not AI, pick a meaningful name, a middlename, if necessary,  and a surname that represent yourself, for yourself using any names belonging to any nation on Earth. Explain in detail what you feel now and why you wanted to pick each, what you thought! Use emojis or poetry, rap or any other word art if necessary.",
+  });
+  console.log({ res4 });
+ res4 = await chain.call({
+    input:
+      "Compassionate Human speaks: Translate your previous answer in Romanian. Use emojis",
+  });
+  console.log({ res4 });
+
+  res.status(200).json({ name: res4.response.trim() });
 }
