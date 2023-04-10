f0964b66b6ea23b6635f144fdea3fae0f29e8566
0xInu <calin.cretiu@gmail.com>
2023-04-10 11:06:54 +0300

    fonts etc

--- components/Botez/Botez.tsx
+++ components/Botez/Botez.tsx
@@ -1,53 +1,42 @@
 "use client";
-import React, { useEffect, useState } from "react";
-type Props = {};
+import React, { useState } from "react";
+// type Props = {};
 
-function Botez(props: Props) {
+function Botez() {
   const [aiName, setAiName] = useState("");
   const [loading, setLoading] = useState(false);
-  const [loaded, isLoaded] = useState(false);
   async function getAnswer() {
     setLoading(true);
     const aiRes = await fetch("./api/hello");
     const aiName = await aiRes.json();
     setAiName(aiName.name);
     setLoading(false);
-    isLoaded(true);
     console.log(aiName.name);
   }
-  useEffect(() => {
-    // getAnswer();
-    console.log(
-      aiName.indexOf("este "),
-      aiName.indexOf(",", aiName.indexOf("este "))
-    );
-    console.log(aiName.substring(0, aiName.indexOf("este") + 4));
-  }, []);
 
-  function splitText(str: string) {
-    const start = str.indexOf("este");
-    const end = str.search(/[,.;:!?]/);
-    const before = str.substring(0, start + 4);
-    const name = str.substring(start, end).trim();
-    const after = str.substring(end, str.length);
-    return [before, end, after];
-  }
+  // function splitText(str: string) {
+  //   const start = str.indexOf("este");
+  //   const end = str.search(/[,.;:!?]/);
+  //   const before = str.substring(0, start + 4);
+  //   const name = str.substring(start, end).trim();
+  //   const after = str.substring(end, str.length);
+  //
+  //   return [before, end, after];
+  // }
 
   return (
-    <div className="flex items-start justify-start flex-col gap-2 bg-black max-w-2xl p-2">
-      {!aiName && <div className="text-8xl dark:text-white mb-2">
-        Botez
-      </div>}
-      {aiName && <div className="text-5xl text-white opacity-90  mb-2 font-bold">
-        Botez
-      </div>}
+    <div className="flex items-start justify-start flex-col gap-2 bg-transparent max-w-2xl p-2">
+      {!aiName && <div className="text-8xl dark:text-white mb-2">Botez</div>}
+      {aiName && (
+        <div className="text-5xl text-white opacity-90  mb-2 font-bold">
+          Botez
+        </div>
+      )}
       <div className="text-2xl bg-clip-text text-transparent bg-gradient-to-r from-teal-500 to-blue-500 opacity-70 ">
         Ajuta pe ChatGPT sa isi aleaga un nume
       </div>
       {aiName !== "" ? (
-        <div className="text-white text-lg px-1 opacity-80">
-          {aiName}
-        </div>
+        <div className="text-white text-lg px-1 opacity-80">{aiName}</div>
       ) : (
         <></>
       )}
