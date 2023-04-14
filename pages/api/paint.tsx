51c5c2d751f932bd06d0d3eb7faf15865fb93228
0xInu <calin.cretiu@gmail.com>
2023-04-14 15:43:08 +0300

    proompt

--- pages/api/paint.tsx
+++ pages/api/paint.tsx
@@ -15,8 +15,8 @@ export default async function handler(
     "tstramer/midjourney-diffusion:436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b";
   const input = {
     prompt: req.body.key.response,
-    negative_prompt:
-      "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs...,blurry image, watermark, messy",
+    // negative_prompt:
+      // "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs...,blurry image, watermark, messy",
   };
   const output: any = await replicate.run(model, { input });
 
